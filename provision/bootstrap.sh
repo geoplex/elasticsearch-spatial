a9ca25708abd1a6eca4c290c5860a884a10d621b
Jerome Anthony <jerome.pradeep@gmail.com>
2014-09-09 15:29:15 -0700

    Fix cloning and installing dependancies for es spatial tutorial

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 # Update the environment and install helpful tools.
-sudo python-software-properties -y
+sudo grub-install /dev/sda
+sudo update-grub
+# sudo apt-get -y install grub-pc
+sudo apt-get install python-software-properties -y
 sudo apt-add-repository ppa:ubuntugis/ppa -y
 sudo apt-get update
-sudo apt-get upgrade gcc -y
+#sudo apt-get upgrade gcc -y
 sudo apt-get install curl -y
 sudo apt-get install vim -y
 sudo apt-get install python-dev -y
@@ -15,6 +18,28 @@ sudo apt-get install unzip -y
 
 # Install a java runtime for elastic search.
 sudo apt-get install openjdk-7-jre-headless -y
+
+# Checkout tutorial project
+
+mkdir es-tutorial
+cd es-tutorial/
+git clone https://github.com/geoplex/elasticsearch-spatial.git
+ls
+cd elasticsearch-spatial/
+# Install python modules for loading data.
+virtualenv LOADER
+source LOADER/bin/activate
+pip install numpy
+pip install cython
+pip install fiona
+pip install pyes
+pip install shapely
+
+# Unzip tutorial data.
+cd exercise_data/
+unzip Melbourne-Localities.zip
+unzip Melbourne_accident.zip
+cd ../..
 
 # Install elastic search and necessary plugins.
 mkdir elasticsearch
@@ -47,23 +72,3 @@ sudo mv /etc/nginx/sites-available/default ~/nginx-default-config
 sudo cp /vagrant/provision/config/kibana.nginx-config /etc/nginx/sites-available/kibana
 sudo ln -s /etc/nginx/sites-available/kibana /etc/nginx/sites-enabled/kibana
 sudo service nginx restart
-
-# Checkout tutorial project
-cd ~
-git clone https://github.com/geoplex/elasticsearch-spatial.git
-cd elasticsearch-spatial
-
-# Install python modules for loading data.
-virtualenv LOADER
-source LOADER/bin/activate
-pip install numpy
-pip install cython
-pip install fiona
-pip install pyes
-pip install shapely
-
-# Unzip tutorial data.
-cd exercise_data/
-unzip Melbourne-Localities.zip
-unzip Melbourne_accident.zip
-cd ..
