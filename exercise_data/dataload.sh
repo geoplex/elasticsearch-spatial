1c3a8eeec24ae4bd960ffa6f1be5c7e143b16823
simon hope <shope@geoplex.com.au>
2014-09-09 19:20:50 -0700

    moving data load

--- exercise_data/dataload.sh
+++ exercise_data/dataload.sh
@@ -6,16 +6,9 @@ curl -XPUT '127.0.01:9200/suburbs/_mapping/suburb' --data @../mappings/suburb_ma
 curl -XPUT 'http://127.0.01:9200/accidents/'
 curl -XPUT '127.0.01:9200/accidents/_mapping/accident' --data @../mappings/accident_mapping.json
 
-# Figureout fiona and shapely dependancies.	
-virtualenv LOADER
+#source
 source LOADER/bin/activate
-pip install pyes
-pip install fiona
-pip install shapely	
 
-#Unzip data
-unzip Melbourne-Localities.zip
-unzip Melbourne-accident.zip
-
+#load
 python ../loader/data-loader.py '127.0.0.1:9200' 'suburbs'  'suburb' '../exercise_data/Melbourne-Localities/melbourne_locality_polygon.shp' 'id' --limit 1000
 python ../loader/data-loader.py '127.0.0.1:9200' 'accidents'  'accident' '../exercise_data/Melbourne_accident/melbourne_accident.shp' 'id' --limit 50000
